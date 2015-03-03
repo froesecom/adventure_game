2f81dc0f28af80500f1bb120f229f8730a3fdbef
Erik Froese <erik@hhd.com.au>
2015-03-03 16:59:05 +1100

    improve each function

--- javascripts/mechanics.js
+++ javascripts/mechanics.js
@@ -2,9 +2,9 @@
 function type(string){
   chars = string.split("");
   $("#adventure_content").text("");
-  $.each(chars, function(i){
-    var l = chars.shift();
-    setTimeout(function(){$("#adventure_content").append(l);}, i*30);
+  $.each(chars, function(i, val){
+    // var l = chars.shift();
+    setTimeout(function(){$("#adventure_content").append(val);}, i*30);
   });
   
 }
